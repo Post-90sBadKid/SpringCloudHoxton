b23df671100600c502c6aa384df387b9bbe7d9cd
Mr.Wang <wry10150@163.com>
2020-03-30 21:39:31 +0800

    模块名称修改

---
+++ cloud-provider-consul-payment8006/src/main/java/com/wry/springcloud/PaymentMain8006.java
@@ -0,0 +1,15 @@
+package com.wry.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wry
+ */
+@SpringBootApplication
+public class PaymentMain8006 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
